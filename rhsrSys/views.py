d787a38d13731a01fd1fcee3c8dcea4b6d773ab0
roobeen-b <rubinbaidhya@gmail.com>
2022-10-28 13:36:14 +0545

    Backend initiation

--- rhsrSys/views.py
+++ rhsrSys/views.py
@@ -1,6 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rhsrSys.functions import handle_uploaded_file  
+from rhsrSys.forms import UploadForm  
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':  
+        student = UploadForm(request.POST, request.FILES)  
+        if student.is_valid():  
+            handle_uploaded_file(request.FILES['file'])  
+            return render(request,"rhsrSys/recognize.html")
+    else:  
+        student = UploadForm()  
+        return render(request,"rhsrSys/index.html",{'form':student})
 
-    return render(request, 'rhsrSys/index.html')
+def recognize(request):
+    return HttpResponse('DOne')
